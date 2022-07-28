446e738be38f57f1f51d7517eb8f6c76214f02aa
GRASSETS TECH LIMITED <82155440+grassets-tech@users.noreply.github.com>
2022-07-28 01:31:38 +0100

    Create klaytn-sync-check.sh

---
+++ tools/klaytn-sync-check.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+### IMPORTANT NOTES ###
+### Prerequisite: curl and jq ###
+
+remote_endpont="https://public-node-api.klaytnapi.com/v1/cypress"
+local_endpoint="http://127.0.0.1:31271"
+telegram_chat_id="CHAT-ID"
+telegram_bot="BOT-ID"
+alert_message="Klaytn Node block difference is: "
+block_diff_threshold="50"
+check_interval="5"
+
+while true
+do
+  remote_height=$(($(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $remote_endpont | jq -r .result)))
+  local_height=$(($(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $local_endpoint | jq -r .result)))
+
+  re='^[0-9]+$'
+  if ! [[ $remote_height =~ $re ]] ; then
+     echo "error: remote_height is not a number" >&2; exit 1
+  fi
+
+  if ! [[ $local_height =~ $re ]] ; then
+     echo "error: local_height not a number" >&2; exit 1
+  fi
+
+  echo "Publick block height is " $remote_height
+  echo "Local block height is " $local_height
+  echo "Difference is " `expr $remote_height - $local_height`
+  blockdiff=$(expr $remote_height - $local_height)
+
+  if [[ $blockdiff -gt $block_diff_threshold ]]; then
+	echo "WARNING: block height is different" \
+	&& curl -s -X POST https://api.telegram.org/$telegram_bot/sendMessage -d chat_id=$telegram_chat_id -d text="$(echo -e $alert_message $blockdiff)";
+        ## && sudo systemctl restart klaytn.service
+  else
+        echo "We are doing fine this time, lets check in "$check_interval "seconds" ;
+  fi
+  sleep $check_interval
+done
