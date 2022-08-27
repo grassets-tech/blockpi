885ccf53585c5e19d7889554072165cdaf1233eb
GRASSETS TECH LIMITED <82155440+grassets-tech@users.noreply.github.com>
2022-08-27 22:06:01 +0100

    Update polygon-block-check.sh

--- tools/polygon-block-check.sh
+++ tools/polygon-block-check.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 ### IMPORTANT NOTES ###
-### curl and jq needs to be installed in advance ###
+### Prerequisite: curl and jq ###
 
-### variables section ###
 remote_endpont="https://polygon-rpc.com"
-local_endpoint="http://127.0.0.1:31291"
-telegram_chat_id="chat-id"
-telegram_bot="bot-id"
-alert_message="Polygon Node block difference is "
+local_endpoint="http://127.0.0.1:31271"
+telegram_chat_id="CHAT-ID"
+telegram_bot="BOT-ID"
+alert_message="Polygon Node block difference is: "
 block_diff_threshold="10"
 check_interval="5"
 
-### main logic ###
 while true
 do
   remote_height=$(($(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $remote_endpont | jq -r .result)))
   local_height=$(($(curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $local_endpoint | jq -r .result)))
-
   re='^[0-9]+$'
   if ! [[ $remote_height =~ $re ]] ; then
      echo "error: remote_height is not a number" >&2; exit 1
@@ -27,15 +24,14 @@ do
      echo "error: local_height not a number" >&2; exit 1
   fi
 
-  echo "Polygon's block height is " $remote_height
+  echo "Public block height is " $remote_height
   echo "Local block height is " $local_height
   echo "Difference is " `expr $remote_height - $local_height`
   blockdiff=$(expr $remote_height - $local_height)
 
   if [[ $blockdiff -gt $block_diff_threshold ]]; then
-	echo "Houston, we have a problem" \
+	echo "WARNING: block height is different, sending Alert" \
 	&& curl -s -X POST https://api.telegram.org/$telegram_bot/sendMessage -d chat_id=$telegram_chat_id -d text="$(echo -e $alert_message $blockdiff)";
-        ## && sudo systemctl restart klaytn.service
   else
         echo "We are doing fine this time, lets check in "$check_interval "seconds" ;
   fi
